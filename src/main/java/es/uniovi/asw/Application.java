11b12b8f825cd7557a1cc48d86187ab13a69effe
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2017-04-22 15:01:40 +0200

    Añadiendo wiki y ordenando ficheros

--- src/main/java/es/uniovi/asw/Application.java
+++ src/main/java/es/uniovi/asw/Application.java
@@ -2,10 +2,9 @@ package es.uniovi.asw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 
 @SpringBootApplication
-public class Application extends AsyncConfigurerSupport {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
