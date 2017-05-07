e9d4bff87c6bcd3dbe9452045869e256cf40bb8e
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2017-05-07 17:28:45 +0200

    asd

--- Prueba.sh
+++ Prueba.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+if [ ! -d kafka ]; then
+    wget http://apache.rediris.es/kafka/0.10.2.0/kafka_2.11-0.10.2.0.tgz -O kafka.tgz
+fi
 mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
 nohup bash -c "cd kafka && bin/zookeeper-server-start.sh config/zookeeper.properties &"
 nohup bash -c "cd kafka && bin/kafka-server-start.sh config/server.properties &"
