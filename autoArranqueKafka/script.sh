970f44c60f01b368435270175f19ceaf6db3ca71
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2017-05-07 19:48:38 +0200

    asdsa2

--- autoArranqueKafka/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-gnome-terminal -e "bash bin/zookeeper-server-start.sh config/zookeeper.properties"
-sleep 10s
-gnome-terminal -e "bash bin/kafka-server-start.sh config/server.properties"
-
