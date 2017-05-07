64499b593724c9927764d9ecc77ea16eba2c924b
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2017-05-07 17:21:52 +0200

    ada

--- Prueba.sh
+++ Prueba.sh
@@ -3,4 +3,5 @@ wget http://apache.rediris.es/kafka/0.10.2.0/kafka_2.11-0.10.2.0.tgz -O kafka.tg
 mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
 nohup bash -c "cd kafka && bin/zookeeper-server-start.sh config/zookeeper.properties &"
 nohup bash -c "cd kafka && bin/kafka-server-start.sh config/server.properties &"
+sleep -s 10
 java $JAVA_OPTS -Dserver.port=$PORT -jar target/*.jar  
